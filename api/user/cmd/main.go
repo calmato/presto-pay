00552c597c02ef363c53b404563e2900638906be
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-05 14:39:01 +0900

    feat: Setup logger

--- api/user/cmd/main.go
+++ api/user/cmd/main.go
@@ -4,12 +4,22 @@ import (
 	"net/http"
 
 	"github.com/calmato/presto-pay/api/user/config"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	// ログ出力設定
+	config.Logger()
+
+	// 環境変数
+	e, err := config.LoadEnvironment()
+	if err != nil {
+		log.Panic(err)
+	}
+
 	// サーバ起動
 	r := config.Router()
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		panic(err)
+	if err := http.ListenAndServe(":"+e.Port, r); err != nil {
+		log.Panic(err)
 	}
 }
