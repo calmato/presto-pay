696274b7fedb9630fca8877bb6e956803cb347eb
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-06 15:00:42 +0900

    fix: Update environment.go

--- api/user/config/environment.go
+++ api/user/config/environment.go
@@ -7,7 +7,8 @@ import (
 
 // Environment - システム内で利用する環境変数の構造体
 type Environment struct {
-	Port string `envconfig:"PORT" default:"8080"`
+	Port                         string `envconfig:"PORT" default:"8080"`
+	GoogleApplicationCredentials string `envconfig:"GOOGLE_APPLICATION_CREDENTIALS" required:"true"`
 }
 
 // LoadEnvironment - 環境変数の取得
