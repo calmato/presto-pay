85355fdce3f1398e48ebbe3a7b146d13ff3f0ae4
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-06 15:47:01 +0900

    feat: Update .env

--- api/user/config/environment.go
+++ api/user/config/environment.go
@@ -9,6 +9,7 @@ import (
 type Environment struct {
 	Port                         string `envconfig:"PORT" default:"8080"`
 	GoogleApplicationCredentials string `envconfig:"GOOGLE_APPLICATION_CREDENTIALS" required:"true"`
+	GCPStorageBucketName         string `envconfig:"GCP_STORAGE_BUCKET_NAME" required:"true"`
 }
 
 // LoadEnvironment - 環境変数の取得
