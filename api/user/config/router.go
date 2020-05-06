93d42ba4ae7c23a735425e306a8ad838c5297875
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-06 13:05:33 +0900

    feat: Update routes

--- api/user/config/router.go
+++ api/user/config/router.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"github.com/calmato/presto-pay/api/user/internal/domain"
+	"github.com/calmato/presto-pay/api/user/internal/interface/handler"
 	"github.com/calmato/presto-pay/api/user/registry"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/xerrors"
 )
 
 // Router - ルーティングの定義
@@ -15,6 +18,11 @@ func Router(reg *registry.Registry) *gin.Engine {
 	// TODO: Logging設定
 
 	r.GET("/health", reg.Health.HealthCheck)
+
+	r.NoRoute(func(c *gin.Context) {
+		err := xerrors.New("NotFound")
+		handler.ErrorHandling(c, domain.NotFound.New(err))
+	})
 
 	return r
 }
