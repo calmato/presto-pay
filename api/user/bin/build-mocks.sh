3e4f248701db25b0b5466e7202732a16ab55197a
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-07 09:09:47 +0900

    feat: Create mocks

--- api/user/bin/build-mocks.sh
+++ api/user/bin/build-mocks.sh
@@ -7,20 +7,47 @@ if [ ! -f /go/bin/mockgen ]; then
 fi
 
 #############################
+# Variables
+#############################
+ignore_lists='test|base|domain/validation.go'
+
+#############################
 # Function
 #############################
 build_mock() {
-  dir_name=$1
-  target=$2
+  echo '- start -----'
+  echo "target: $1"
+  echo '-------------'
 
-  paths=$(find internal/${dir_name} -name ${target} \
-    | grep -vE 'test|base' \
-    | awk -v var=${dir_name} '{ print substr($0, index($0, var)) }') \
-    | sed -e "s/\/${target}$//"
+  target=$1
+  mockgen -source internal/${target} -destination mock/${target}
+}
+
+domain() {
+  dir_name='domain'
+  file_name=$1
+
+  paths=$(find internal/${dir_name} -name ${file_name} \
+    | grep -vE ${ignore_lists} \
+    | awk -v var=${dir_name} '{ print substr($0, index($0, var)) }' \
+    | sed -e "s/\/${file_name}$//")
 
   for path in ${paths}; do
-    mkdir -p mocks/${path}
-    mockgen -source internal/${path}/${target} -destination mock/${path}/${target}
+    mkdir -p mock/${path}
+    build_mock "${path}/${file_name}"
+  done
+}
+
+application() {
+  dir_name="application/$1"
+  mkdir -p mock/${dir_name}
+
+  paths=$(find internal/${dir_name} -name '*.go' \
+    | grep -vE ${ignore_lists} \
+    | awk -v var=${dir_name} '{ print substr($0, index($0, var)) }')
+
+  for path in ${paths}; do
+    build_mock ${path}
   done
 }
 
@@ -28,10 +55,10 @@ build_mock() {
 # Target
 #############################
 # --- Domain ---
-build_mock 'domain' 'repository.go'
-build_mock 'domain' 'service.go'
-build_mock 'domain' 'uploader.go'
-build_mock 'domain' 'validation.go'
+domain 'repository.go'
+domain 'service.go'
+domain 'uploader.go'
+domain 'validation.go'
 
 # --- Application ---
-build_mock 'application/validation' '*.go'
+application 'validation'
