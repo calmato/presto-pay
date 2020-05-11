40beb993128bbbbebcc313592e637ef25f42a9fd
濵田広大 <hamany0326@gmail.com>
2020-05-11 21:56:20 +0900

    アンダーバーの追加

--- native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
@@ -2,11 +2,14 @@ package work.calmato.prestopay
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.SpannableString
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.fragment_login)
+
   }
 }
