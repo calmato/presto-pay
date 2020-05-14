def709b8c16614b0789643258fcaa29ebea060ff
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-15 01:37:00 +0900

    feat: Update error message

--- api/user/internal/application/response/error.go
+++ api/user/internal/application/response/error.go
@@ -20,41 +20,56 @@ type ErrorResponse struct {
 	ValidationErrors []*ValidationError `json:"errors,omitempty"`
 }
 
+// エラーメッセージ
+const (
+	badRequestMessage = `The API request is invalid or improperly formed. Consequently,
+the API server could not understand the request.`
+	unauthorizedMessage = "The user is not authorized to make the request."
+	forbiddenMessage    = "The requested operation is forbidden and cannot be completed."
+	notFoundMessage     = `The requested operation failed
+because a resource associated with the request could not be found.`
+	alreadyExistsMessage = `The API request cannot be completed
+because the requested operation would conflict with an existing item.
+For example, a request that tries to create a duplicate item would create a conflict,
+though duplicate items are typically identified with more specific errors.`
+	internalServerErrorMessage = "The request failed due to an internal error."
+)
+
 // ステータスコードを付与したエラーレスポンス
 var (
 	BadRequest = &ErrorResponse{
 		StatusCode:       http.StatusBadRequest, // 400
-		Message:          "不正なパラメータが入力されています。",
+		Message:          badRequestMessage,
 		ValidationErrors: nil,
 	}
 
 	Unauthorized = &ErrorResponse{
 		StatusCode:       http.StatusUnauthorized, // 401
-		Message:          "認証に必要な情報がありません。",
+		Message:          unauthorizedMessage,
 		ValidationErrors: nil,
 	}
 
 	Forbidden = &ErrorResponse{
 		StatusCode:       http.StatusForbidden, // 403
-		Message:          "その操作を実行する権限がありません。",
+		Message:          forbiddenMessage,
 		ValidationErrors: nil,
 	}
 
 	NotFound = &ErrorResponse{
 		StatusCode:       http.StatusNotFound, // 404
-		Message:          "指定の情報が見つかりません。",
+		Message:          notFoundMessage,
 		ValidationErrors: nil,
 	}
 
 	AlreadyExists = &ErrorResponse{
 		StatusCode:       http.StatusConflict, // 409
-		Message:          "不正なパラメータが入力されています。",
+		Message:          alreadyExistsMessage,
 		ValidationErrors: nil,
 	}
 
 	InternalServerError = &ErrorResponse{
 		StatusCode:       http.StatusInternalServerError, // 500
-		Message:          "異常な処理が検出されました。",
+		Message:          internalServerErrorMessage,
 		ValidationErrors: nil,
 	}
 )
