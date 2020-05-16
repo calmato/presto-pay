8358ffcf5ea161990bbd82f44e1fb63e290bf872
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-05-17 01:14:46 +0900

    feat: Add root view for routing

---
+++ native/ios/PrestoPay/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrestoPay
+//
+//  Created by 西川　直志 on 2020/05/17.
+//  Copyright © 2020 calmato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    LoginView()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
