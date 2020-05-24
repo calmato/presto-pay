b31e718ca131934abd1bfe8f036b76527910dd33
Atsuhide <atsuhide.i@gmail.com>
2020-05-24 23:12:53 +0900

    アカウント作成の画像をギャラリーから追加できるようにした。 ギャラリーへのアクセス権限をユーザーに求めるようにした。

--- native/android/app/src/main/java/work/calmato/prestopay/ui/login/CreateNewAccount.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/login/CreateNewAccount.kt
@@ -1,23 +1,35 @@
 package work.calmato.prestopay.ui.login
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_create_new_account.*
 import work.calmato.prestopay.R
 
+
 class CreateNewAccount : AppCompatActivity() {
   val serverUrl: String = "https://api.presto-pay-stg.calmato.work/v1/users"
   var jsonText: String = ""
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.fragment_create_new_account)
 
     val createButton: Button = findViewById(R.id.createAccountButton)
+
     // buttonを押した時の処理を記述
     createButton.setOnClickListener {
       val thumbnails = ""
@@ -45,12 +57,80 @@ class CreateNewAccount : AppCompatActivity() {
         println("The password and the confirmation password did not match.")
       }
     }
+
+    editPhotoText.setOnClickListener{
+      //check runtime permission
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+        // Permission is not granted
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+          // Show an explanation to the user *asynchronously* -- don't block
+          // this thread waiting for the user's response! After the user
+          // sees the explanation, try again to request the permission.
+          Toast.makeText(this,"設定からギャラリーへのアクセスを許可してください",Toast.LENGTH_LONG).show()
+        } else {
+          // No explanation needed, we can request the permission.
+          ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
+        }
+      }
+      else{
+        //permission already granted
+        pickImageFromGallery()
+      }
+    }
+    hasAccountText.setOnClickListener{
+    }
   }
 
   inner class MyAsyncTask: AsyncTask<Void, Void, String>() {
     override fun doInBackground(vararg params: Void?): String? {
       RestClient().postExecute(jsonText, serverUrl)
       return RestClient().getResult()
+    }
+  }
+
+
+  companion object {
+    private val IMAGE_PICK_CODE = 1000
+    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1001
+  }
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<String>,
+    grantResults: IntArray
+  ) {
+    when(requestCode){
+      MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
+        // If request is cancelled, the result arrays are empty.
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+          pickImageFromGallery()
+        } else {
+          Toast.makeText(this,"permission denied",Toast.LENGTH_LONG).show()
+        }
+        return
+      }
+      else -> {
+        // Ignore all other requests.
+      }
+    }
+  }
+
+  private fun pickImageFromGallery() {
+    //Intent to pick image
+    val intent = Intent(Intent.ACTION_PICK)
+    intent.type = "image/*"
+    startActivityForResult(intent, IMAGE_PICK_CODE)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+      thumbnail.setImageURI(data?.data)
+      thumbnail.setBackgroundColor(Color.TRANSPARENT)
+      editPhotoText.setText("写真を変更")
     }
   }
 
