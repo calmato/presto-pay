c0348eb158f9a2b1a08e7dc6b4983e235af3e836
koudai hamada <e155412k@st.u-gakugei.ac.jp>
2020-05-25 23:54:07 +0900

    Revert "ナビゲーション＆ディレクトリわけ"

--- native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
@@ -1,14 +1,13 @@
 package work.calmato.prestopay
 
-
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.fragment_notification_setting)
 
   }
 }
