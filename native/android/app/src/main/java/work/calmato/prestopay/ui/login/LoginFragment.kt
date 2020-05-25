d0abc8d4458442bd27ba8ca5a6a53dd8d286b16e
Atsuhide <atsuhide.i@gmail.com>
2020-05-25 23:13:23 +0900

    アカウントをお持ちでない方をクリックするとアカウント作成画面に飛ぶようにした。 今後画面のボタンとかをKotlinで扱うときはbindingされてるのでfindViewByIdは必要なし。 ことりんファイルに紐付けられているXMLで定義されたIDをそのまま呼び出せるはず。

--- native/android/app/src/main/java/work/calmato/prestopay/ui/login/LoginFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/login/LoginFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
 import work.calmato.prestopay.R
 import work.calmato.prestopay.databinding.FragmentLoginBinding
 
@@ -16,8 +20,18 @@ class LoginFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    val binding:FragmentLoginBinding = DataBindingUtil.inflate(
-      inflater, R.layout.fragment_login,container,false)
+    val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+      inflater, R.layout.fragment_login, container, false
+    )
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    loginNewText.setOnClickListener {
+      this.findNavController().navigate(
+        LoginFragmentDirections.actionLoginFragmentToNewAccountFragment()
+      )
+    }
   }
 }
