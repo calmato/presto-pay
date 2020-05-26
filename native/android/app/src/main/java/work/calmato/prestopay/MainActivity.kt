0d953e8eb26596b449c968736abd218a7e669e2a
koudai hamada <e155412k@st.u-gakugei.ac.jp>
2020-05-26 21:45:28 +0900

    Revert "restore branch"

--- native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
@@ -8,6 +8,5 @@ class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    
   }
 }
