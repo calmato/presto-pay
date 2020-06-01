12941b88aed7c445e83c910b23bd8a52341404a6
濵田広大 <hamany0326@gmail.com>
2020-06-02 00:57:07 +0900

    firebase でのtokenの取得と永続化

--- native/android/app/src/main/java/work/calmato/prestopay/ui/login/LoginFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/login/LoginFragment.kt
@@ -1,21 +1,10 @@
 package work.calmato.prestopay.ui.login
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-
 import androidx.fragment.app.Fragment
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
-import work.calmato.prestopay.R
-import work.calmato.prestopay.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
-  override fun onCreateView(
+/*  override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
@@ -24,14 +13,14 @@ class LoginFragment : Fragment() {
       inflater, R.layout.fragment_login, container, false
     )
     return binding.root
-  }
+  }*/
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     loginNewText.setOnClickListener {
       this.findNavController().navigate(
         LoginFragmentDirections.actionLoginFragmentToNewAccountFragment()
       )
     }
-  }
+  }*/
 }
