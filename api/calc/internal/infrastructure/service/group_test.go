447130f1009c70a2e38f22ab44127f7d21e22c3a
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-06-12 00:17:04 +0900

    test: group service

--- api/calc/internal/infrastructure/service/group_test.go
+++ api/calc/internal/infrastructure/service/group_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	"github.com/calmato/presto-pay/api/calc/internal/domain/group"
@@ -56,6 +57,52 @@ func TestGroupService_Create(t *testing.T) {
 			_, err := target.Create(ctx, testCase.Group)
 			if err != nil {
 				t.Fatalf("error: %v", err)
+				return
+			}
+		})
+	}
+}
+
+func TestGroupService_UploadThumbnail(t *testing.T) {
+	testCases := map[string]struct {
+		Data     []byte
+		Expected string
+	}{
+		"ok": {
+			Data:     []byte{0xDe, 0xaD, 0xBe, 0xeF},
+			Expected: "http://localhost:8080",
+		},
+	}
+
+	for result, testCase := range testCases {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		// Defined mocks
+		gdvm := mock_group.NewMockGroupDomainValidation(ctrl)
+
+		grm := mock_group.NewMockGroupRepository(ctrl)
+
+		gum := mock_group.NewMockGroupUploader(ctrl)
+		gum.EXPECT().UploadThumbnail(ctx, testCase.Data).Return(testCase.Expected, nil)
+
+		acm := mock_api.NewMockAPIClient(ctrl)
+
+		// Start test
+		t.Run(result, func(t *testing.T) {
+			target := NewGroupService(gdvm, grm, gum, acm)
+
+			got, err := target.UploadThumbnail(ctx, testCase.Data)
+			if err != nil {
+				t.Fatalf("error: %v", err)
+				return
+			}
+
+			if !reflect.DeepEqual(got, testCase.Expected) {
+				t.Fatalf("want %#v, but %#v", testCase.Expected, got)
 				return
 			}
 		})
