b32c6286038867caded1c91f7817da51635cf0ba
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-06-12 00:19:18 +0900

    test: group request validation

---
+++ api/calc/internal/application/validation/group_test.go
@@ -0,0 +1,38 @@
+package validation
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/calmato/presto-pay/api/calc/internal/application/request"
+	"github.com/calmato/presto-pay/api/calc/internal/domain"
+)
+
+func TestGroupRequestValidation_CreateGroup(t *testing.T) {
+	testCases := map[string]struct {
+		Request  *request.CreateGroup
+		Expected []*domain.ValidationError
+	}{
+		"ok": {
+			Request: &request.CreateGroup{
+				Name:      "テストユーザー",
+				Thumbnail: "",
+				UserIDs:   []string{},
+			},
+			Expected: make([]*domain.ValidationError, 0),
+		},
+	}
+
+	for result, testCase := range testCases {
+		// Start test
+		t.Run(result, func(t *testing.T) {
+			target := NewGroupRequestValidation()
+
+			got := target.CreateGroup(testCase.Request)
+			if !reflect.DeepEqual(got, testCase.Expected) {
+				t.Fatalf("want %#v, but %#v", testCase.Expected, got)
+				return
+			}
+		})
+	}
+}
