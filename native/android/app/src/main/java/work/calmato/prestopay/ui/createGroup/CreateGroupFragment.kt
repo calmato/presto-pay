bd94d66a76b60f2ada8b9e878c62ac8f74886fd0
Atsuhide <58492130+Atsuhide@users.noreply.github.com>
2020-07-04 22:39:33 +0900

    Update CreateGroupFragment.kt

--- native/android/app/src/main/java/work/calmato/prestopay/ui/createGroup/CreateGroupFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/createGroup/CreateGroupFragment.kt
@@ -13,7 +13,6 @@ import android.view.*
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.firebase.auth.FirebaseAuth
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.Gson
