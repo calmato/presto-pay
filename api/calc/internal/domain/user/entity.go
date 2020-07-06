2eed0a4ba4192dad8eebf349f37619d26a11f29d
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2020-07-06 23:06:21 +0900

    feat: Update user domain

--- api/calc/internal/domain/user/entity.go
+++ api/calc/internal/domain/user/entity.go
@@ -4,13 +4,15 @@ import "time"
 
 // User - Userエンティティ
 type User struct {
-	ID           string    `json:"id"`           // Firebase AuthenticationのUID
-	Name         string    `json:"name"`         // 表示名
-	Username     string    `json:"username"`     // ユーザー名 [unique]
-	Email        string    `json:"email"`        // メールアドレス
-	ThumbnailURL string    `json:"thumbnailUrl"` // サムネイル
-	GroupIDs     []string  `json:"groupIds"`     // グループID一覧
-	Password     string    `json:"-"`            // パスワード
-	CreatedAt    time.Time `json:"createdAt"`    // 作成日時
-	UpdatedAt    time.Time `json:"updatedAt"`    // 更新日時
+	ID            string    `json:"id"`                  // Firebase AuthenticationのUID
+	Name          string    `json:"name"`                // 表示名
+	Username      string    `json:"username"`            // ユーザー名 [unique]
+	UsernameLower string    `firestore:"username_lower"` // ユーザー名 (すべて小文字)
+	Email         string    `json:"email"`               // メールアドレス
+	ThumbnailURL  string    `json:"thumbnailUrl"`        // サムネイル
+	GroupIDs      []string  `json:"groupIds"`            // グループID一覧
+	FriendIDs     []string  `firestore:"friend_ids"`     // 友達のユーザーID一覧
+	Password      string    `json:"-"`                   // パスワード
+	CreatedAt     time.Time `json:"createdAt"`           // 作成日時
+	UpdatedAt     time.Time `json:"updatedAt"`           // 更新日時
 }
