b10273d1a4b0cd8fa1a1407bb9f803f306e062c2
Atsuhide <atsuhide.i@gmail.com>
2020-07-07 22:32:56 +0900

    友達一覧の表示

--- native/android/app/src/main/java/work/calmato/prestopay/network/UserProperty.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/network/UserProperty.kt
@@ -5,28 +5,28 @@ import com.google.gson.JsonArray
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class UserProperty (
-  val id:String,
-  val name:String,
-  val username:String,
-  val email:String,
-  val thumbnailUrl:String?
-):Parcelable{}
+data class UserProperty(
+  val id: String,
+  val name: String,
+  val username: String,
+  val email: String,
+  val thumbnailUrl: String?
+) : Parcelable {}
 
 @Parcelize
 data class Users(
-  val users:List<UserProperty?>
-):Parcelable {}
+  val users: List<UserProperty?>
+) : Parcelable {}
 
 @Parcelize
 data class addFriendResponse(
-  val id:String,
-  val name:String,
-  val username:String,
-  val email:String,
-  val thumbnailUrl:String?
-):Parcelable{}
+  val id: String,
+  val name: String,
+  val username: String,
+  val email: String,
+  val thumbnailUrl: String?
+) : Parcelable {}
 
 data class UserId(
-  val userId:String
+  val userId: String
 )
