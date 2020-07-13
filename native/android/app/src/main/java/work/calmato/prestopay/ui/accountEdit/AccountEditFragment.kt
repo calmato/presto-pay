0441d7ac6cf0e9fea5f344535c9ee76a36790cc8
Atsuhide <atsuhide.i@gmail.com>
2020-07-13 22:32:20 +0900

    同様に作ったクラスを継承させていらないコードを削除

--- native/android/app/src/main/java/work/calmato/prestopay/ui/accountEdit/AccountEditFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/accountEdit/AccountEditFragment.kt
@@ -27,11 +27,11 @@ import work.calmato.prestopay.network.Api
 import work.calmato.prestopay.network.EditAccountProperty
 import work.calmato.prestopay.network.EditAccountResponse
 import work.calmato.prestopay.util.Constant
+import work.calmato.prestopay.util.PermissionBase
 import work.calmato.prestopay.util.encodeImage2Base64
 import java.lang.Exception
 
-class AccountEditFragment : Fragment() {
-  var setThumbnail = false
+class AccountEditFragment : PermissionBase() {
   var idToken = ""
 
   override fun onCreateView(
@@ -50,10 +50,7 @@ class AccountEditFragment : Fragment() {
 
     //保存buttonを押した時の処理を記述
     savaButton.setOnClickListener {
-      var thumbnails = ""
-      if (setThumbnail) {
-        thumbnails = encodeImage2Base64(thumbnailEdit)
-      }
+      val thumbnails = encodeImage2Base64(thumbnailEdit)
       val name: String = nameEditText.text.toString()
       val userName: String = userNameEditText.text.toString()
       val email: String = mailEditText.text.toString()
@@ -93,72 +90,12 @@ class AccountEditFragment : Fragment() {
     }
 
     thumbnailEdit.setOnClickListener {
-      //check runtime permission
-      if (ContextCompat.checkSelfPermission(
-          requireActivity(),
-          Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-        != PackageManager.PERMISSION_GRANTED
-      ) {
-        // Permission is not granted
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-            requireActivity(),
-            Manifest.permission.READ_EXTERNAL_STORAGE
-          )
-        ) {
-          // Show an explanation to the user *asynchronously* -- don't block
-          // this thread waiting for the user's response! After the user
-          // sees the explanation, try again to request the permission.
-          Toast.makeText(requireActivity(), "設定からギャラリーへのアクセスを許可してください", Toast.LENGTH_LONG).show()
-        } else {
-          // No explanation needed, we can request the permission.
-          ActivityCompat.requestPermissions(
-            requireActivity(),
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-            Constant.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
-          )
-        }
-      } else {
-        //permission already granted
-        pickImageFromGallery()
-      }
+      requestPermission()
     }
     val mUser = FirebaseAuth.getInstance().currentUser
     mUser?.getIdToken(true)?.addOnCompleteListener(requireActivity()) {
       idToken = it.result?.token!!
     }
-  }
-
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<String>,
-    grantResults: IntArray
-  ) {
-    when (requestCode) {
-      Constant.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
-        // If request is cancelled, the result arrays are empty.
-        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-          pickImageFromGallery()
-        } else {
-          Toast.makeText(requireActivity(), "アクセスが拒否されました", Toast.LENGTH_LONG).show()
-        }
-        return
-      }
-      else -> {
-        // Ignore all other requests.
-      }
-    }
-  }
-
-  private fun pickImageFromGallery() {
-    //Intent to pick image
-    val intent = Intent(Intent.ACTION_PICK)
-    intent.type = "image/*"
-    startActivityForResult(
-      intent,
-      Constant.IMAGE_PICK_CODE
-    )
-    setThumbnail = true
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
