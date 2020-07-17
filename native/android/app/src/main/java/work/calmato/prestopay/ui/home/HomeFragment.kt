5c4681071c167d38d2b618097b0a568594c03fec
Atsuhide <atsuhide.i@gmail.com>
2020-07-17 18:42:39 +0900

    修正

--- native/android/app/src/main/java/work/calmato/prestopay/ui/home/HomeFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/home/HomeFragment.kt
@@ -55,7 +55,7 @@ class HomeFragment : Fragment() {
     }
     requireActivity().onBackPressedDispatcher.addCallback(
       viewLifecycleOwner,
-      object : OnBackPressedCallback(false) {
+      object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
         }
       })
