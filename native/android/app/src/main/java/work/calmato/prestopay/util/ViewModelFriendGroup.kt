051e8b7082ca3eacc64f614e00615081feb2e371
Atsuhide <atsuhide.i@gmail.com>
2020-07-26 22:49:50 +0900

    細かい修正

--- native/android/app/src/main/java/work/calmato/prestopay/util/ViewModelFriendGroup.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/util/ViewModelFriendGroup.kt
@@ -22,11 +22,11 @@ class ViewModelFriendGroup(application: Application) : AndroidViewModel(applicat
     get() = _itemClicked
 
   private val _navigateToHome = MutableLiveData<Boolean>()
-  val navigateToHome:LiveData<Boolean>
+  val navigateToHome: LiveData<Boolean>
     get() = _navigateToHome
 
   private val _usersList = MutableLiveData<List<UserProperty>>()
-  val usersList:LiveData<List<UserProperty>>
+  val usersList: LiveData<List<UserProperty>>
     get() = _usersList
 
   // Create a Coroutine scope using a job to be able to cancel when needed
@@ -41,111 +41,91 @@ class ViewModelFriendGroup(application: Application) : AndroidViewModel(applicat
   private val id = sharedPreferences.getString("token", null)
 
   init {
-      viewModelScope.launch {
-        try {
-          friendsRepository.refreshFriends(id!!)
-        } catch (e:java.lang.Exception){
-          Log.i(TAG, "Trying to refresh id")
-        }
+    viewModelScope.launch {
+      try {
+        friendsRepository.refreshFriends(id!!)
+      } catch (e: java.lang.Exception) {
+        Log.i(TAG, "Trying to refresh id")
       }
+    }
   }
 
   val friendsList = friendsRepository.friends
 
-  fun getUserProperties(userName: String,activity: Activity){
+  fun getUserProperties(userName: String, activity: Activity) {
     coroutineScope.launch {
       try {
-      _usersList.value   = Api.retrofitService.getPropertiesAsync("Bearer $id", userName).await().asDomainModel()
-      } catch (e:Exception){
+        _usersList.value =
+          Api.retrofitService.getPropertiesAsync("Bearer $id", userName).await().asDomainModel()
+      } catch (e: Exception) {
         Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
       }
     }
   }
+
   override fun onCleared() {
     super.onCleared()
     viewModelJob.cancel()
   }
 
-  fun addFriendApi(userProperty: UserProperty,activity:Activity){
+  fun addFriendApi(userProperty: UserProperty, activity: Activity) {
     val userId = userProperty.id
     viewModelScope.launch {
       try {
-        friendsRepository.addFriend(id,UserId(userId),userProperty)
+        friendsRepository.addFriend(id, UserId(userId), userProperty)
         Toast.makeText(activity, "友だち追加しました", Toast.LENGTH_SHORT).show()
-      } catch (e:java.lang.Exception){
+      } catch (e: java.lang.Exception) {
         Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
       }
     }
   }
 
-  fun createGroupApi(groupProperty: CreateGroupProperty,activity: Activity) {
-    Api.retrofitService.createGroup("Bearer ${id}", groupProperty).enqueue(object:Callback<CreateGroupPropertyResponse>{
-      override fun onFailure(call: Call<CreateGroupPropertyResponse>, t: Throwable) {
-        Toast.makeText(activity, t.message, Toast.LENGTH_LONG).show()
-      }
-      override fun onResponse(
-        call: Call<CreateGroupPropertyResponse>,
-        response: Response<CreateGroupPropertyResponse>
-      ) {
-        if(response.isSuccessful){
-          Toast.makeText(activity, "新しいグループを作成しました", Toast.LENGTH_SHORT).show()
-          _navigateToHome.value = true
-        }else{
-          try {
-            val jObjError = JSONObject(response.errorBody()?.string()).getJSONArray("errors")
-            for (i in 0 until jObjError.length()) {
-              val errorMessage =
-                jObjError.getJSONObject(i).getString("field") + " " + jObjError.getJSONObject(
-                  i
-                )
-                  .getString("message")
-              Toast.makeText(activity, errorMessage, Toast.LENGTH_LONG).show()
-            }
-          }catch (e: java.lang.Exception){
+  fun createGroupApi(groupProperty: CreateGroupProperty, activity: Activity) {
+    Api.retrofitService.createGroup("Bearer ${id}", groupProperty)
+      .enqueue(object : Callback<CreateGroupPropertyResponse> {
+        override fun onFailure(call: Call<CreateGroupPropertyResponse>, t: Throwable) {
+          Toast.makeText(activity, t.message, Toast.LENGTH_LONG).show()
+        }
+
+        override fun onResponse(
+          call: Call<CreateGroupPropertyResponse>,
+          response: Response<CreateGroupPropertyResponse>
+        ) {
+          if (response.isSuccessful) {
+            Toast.makeText(activity, "新しいグループを作成しました", Toast.LENGTH_SHORT).show()
+            _navigateToHome.value = true
+          } else {
             Toast.makeText(activity, "グループ作成に失敗しました", Toast.LENGTH_LONG).show()
           }
         }
-      }
-    })
+      })
   }
 
-  fun deleteFriend(userId:String, activity: Activity){
-    Api.retrofitService.deleteFriend("Bearer ${id}", userId).enqueue(object:Callback<AccountResponse>{
-      override fun onFailure(call: Call<AccountResponse>, t: Throwable) {
-        Toast.makeText(activity, t.message, Toast.LENGTH_LONG).show()
-      }
+  fun deleteFriend(userId: String, activity: Activity) {
+    Api.retrofitService.deleteFriend("Bearer ${id}", userId)
+      .enqueue(object : Callback<AccountResponse> {
+        override fun onFailure(call: Call<AccountResponse>, t: Throwable) {
+          Toast.makeText(activity, t.message, Toast.LENGTH_LONG).show()
+        }
 
-      override fun onResponse(call: Call<AccountResponse>, response: Response<AccountResponse>) {
-        if(response.isSuccessful){
-          coroutineScope.launch {
-            try {
-              viewModelScope.launch {
-                friendsRepository.deleteFriend(userId)
-                Toast.makeText(activity,"友達リストから削除しました",Toast.LENGTH_LONG).show()
-
+        override fun onResponse(call: Call<AccountResponse>, response: Response<AccountResponse>) {
+          if (response.isSuccessful) {
+            coroutineScope.launch {
+              try {
+                viewModelScope.launch {
+                  friendsRepository.deleteFriend(userId)
+                  Toast.makeText(activity, "友達リストから削除しました", Toast.LENGTH_LONG).show()
+                }
+              } catch (e: java.lang.Exception) {
+                Toast.makeText(activity, "友達削除に失敗しました", Toast.LENGTH_LONG).show()
+                Log.i(TAG, "onResponse: ${e.message}")
               }
-              Log.i(TAG, "getFriends: getUser")
-            }catch (e:java.lang.Exception){
-              Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
             }
-          }
-        }else{
-          try {
-            val jObjError = JSONObject(response.errorBody()?.string()).getJSONArray("errors")
-            for (i in 0 until jObjError.length()) {
-              val errorMessage =
-                jObjError.getJSONObject(i).getString("field") + " " + jObjError.getJSONObject(
-                  i
-                )
-                  .getString("message")
-              Toast.makeText(activity, errorMessage, Toast.LENGTH_LONG).show()
-            }
-          }catch (e: java.lang.Exception){
+          } else {
             Toast.makeText(activity, "友達削除に失敗しました", Toast.LENGTH_LONG).show()
           }
         }
-      }
-    })
+      })
   }
 
   fun itemIsClicked(userProperty: UserProperty) {
@@ -156,7 +136,7 @@ class ViewModelFriendGroup(application: Application) : AndroidViewModel(applicat
     _itemClicked.value = null
   }
 
-  fun navigationCompleted(){
+  fun navigationCompleted() {
     _navigateToHome.value = false
   }
 
