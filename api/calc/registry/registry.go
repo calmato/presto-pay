4a837609cb0e83f36cd86c60b4d021edd0a7ab37
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-08-30 14:31:31 +0900

    feat: Update payment service

--- api/calc/registry/registry.go
+++ api/calc/registry/registry.go
@@ -17,10 +17,10 @@ type Registry struct {
 }
 
 // NewRegistry - internalディレクトリ配下のファイルを読み込み
-func NewRegistry(fs *firestore.Firestore, cs *storage.Storage, _ *messaging.Messaging, ac api.APIClient) *Registry {
+func NewRegistry(fs *firestore.Firestore, cs *storage.Storage, cm *messaging.Messaging, ac api.APIClient) *Registry {
 	health := healthInjection()
 	v1Group := v1GroupInjection(fs, cs, ac)
-	v1Payment := v1PaymentInjection(fs, cs, ac)
+	v1Payment := v1PaymentInjection(fs, cs, cm, ac)
 
 	return &Registry{
 		Health:    health,
