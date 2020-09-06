07fa35ac5db9c8c96c1559b2fe659591d04bfd0f
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-09-07 01:32:12 +0900

    feat: Setup TypeScript

--- native/react/babel.config.js
+++ native/react/babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "~": ["./src/"],
+          "~~": "./",
+        }
+      }
+    ]
+  ]
 };
