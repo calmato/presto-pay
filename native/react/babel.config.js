8a7586de38dcfc8e713d3cc419bc554d23afb007
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-09-06 23:53:28 +0900

    feat: react init

--- native/react/babel.config.js
+++ native/react/babel.config.js
@@ -1,16 +1,6 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '~': ['./src/'],
-          '~~': './',
-        },
-      },
-    ],
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
--- native/react/src/components/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Home} from './Home';
-export {default as Initial} from './Initial';
-export {default as Loading} from './Loading';
-export {default as SignIn} from './SignIn';
