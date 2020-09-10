2b9118e8a5ecaa095e7f09c04f597d0c1f0d01e9
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-09-11 01:12:51 +0900

    feat: Create theme.ts

---
+++ native/react/src/constants/theme.ts
@@ -0,0 +1,11 @@
+export const COLOR = {
+  MAIN: '#FFFFFF', // メインカラー (背景等)
+  DARK: '#23282F', // メインカラー (ダークモード)
+  PRIMARY: '#1976D2', // プライマリーカラー
+  SECONDARY: '#424242',
+  ACCENT: '#82B1FF',
+  ERROR: '#FF5252',
+  INFO: '#2196F3',
+  SUCCESS: '#4CAF50',
+  WARNING: '#FFC107',
+};
