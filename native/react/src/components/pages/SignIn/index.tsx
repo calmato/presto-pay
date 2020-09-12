d3e23ba22478863f42571b39f3a6a2579979a2ba
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-09-13 02:19:43 +0900

    feat: [WIP] update ui

--- native/react/src/components/pages/SignIn/index.tsx
+++ native/react/src/components/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Card } from "react-native-elements";
 
-import { Button } from "~/components/atoms";
+import { Button, TextField } from "~/components/atoms";
 import { SIGN_UP } from "~/constants/path";
 import { COLOR } from "~/constants/theme";
 import { Context, Status } from "~/contexts/ui";
@@ -24,26 +24,32 @@ const styles = StyleSheet.create({
     padding: 10,
     color: COLOR.PRIMARY,
   },
+  link: {
+    padding: 10,
+    textAlign: "center",
+  },
 });
 
 export default function SignIn() {
   const { navigate } = useNavigation();
   const { setApplicationState } = React.useContext(Context);
 
+  // TODO: デザイン, コードリファクタ
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Presto Pay</Text>
       <Card>
+        <TextField label="E-Mail" />
+        <TextField label="Password" secureTextEntry={true} />
+        <Button onPress={() => setApplicationState(Status.AUTHORIZED)} title="ログイン" style={styles.button} />
+        <Text style={styles.link}>パスワードを忘れた方</Text>
+        <Card.Divider />
         <Button onPress={() => console.log("click", "google")} title="Googleでサインイン" style={styles.button} />
         <Button onPress={() => console.log("click", "twitter")} title="Twitterでサインイン" style={styles.button} />
         <Button onPress={() => console.log("click", "facebook")} title="Facebookでサインイン" style={styles.button} />
-        <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
-          <Text>ログイン</Text>
-        </TouchableOpacity>
         <TouchableOpacity onPress={() => navigate(SIGN_UP)}>
-          <Text>新規登録する</Text>
+          <Text style={styles.link}>アカウントをお持ちでない方</Text>
         </TouchableOpacity>
-        <Text>パスワードを忘れた</Text>
       </Card>
     </View>
   );
