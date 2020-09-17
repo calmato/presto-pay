e5d6211aa92c184a919f5dc9098e7c1bd5f6294f
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-09-17 10:44:51 +0900

    feat: propsの型定義とサイズの調整

--- native/react/src/components/organisms/Profile/index.tsx
+++ native/react/src/components/organisms/Profile/index.tsx
@@ -1,40 +1,65 @@
 import React from "react";
-import { Text, View, TextStyle, ViewStyle, StyleSheet } from 'react-native';
-import { Card } from "react-native-elements";
-import { ProfileImage, ProfileUserName } from "~/components/molecules"
+import { Text, View, TextStyle, ViewStyle, StyleSheet } from "react-native";
+
+import { ProfileImage, ProfileUserName } from "~/components/molecules";
+
+// TODO: 型定義を別に切り出す
+interface User {
+  uid: string;
+  name: string;
+  displayName: string;
+  email: string;
+  emailVerified: boolean;
+  phoneNumber: string;
+  password: string;
+  photoURL: string;
+  disabled: boolean;
+}
 
 interface Props {
+  user: User;
 }
 
 const styles = StyleSheet.create({
   card: {
     justifyContent: "center",
     flexDirection: "row",
+    marginTop: 30,
+    marginBottom: 30,
+    alignItems: "center",
   },
   profileImage: {
     flex: 0.4,
   },
   name: {
-    fontSize: 18,
     flex: 0.6,
-    height: 50
+    height: 50,
   },
-})
+});
 
-export default function Profile(props: Props) {
-  // const { onPress, title, style, disabled, disabledStyle, icon } = props;
+const Profile = function Profile(props: Props) {
+  const { user } = props;
 
   return (
-    <Card>
-      <View>
-        <ProfileImage
-          style={styles.profileImage}
-        />
-        <ProfileUserName
-          style={styles.name}
-        />
-      </View>
-    </Card>
+    <View style={styles.card}>
+      <ProfileImage uri={user.photoURL} style={styles.profileImage} />
+      <ProfileUserName name={user.displayName} style={styles.name} />
+    </View>
   );
-}
+};
 
+Profile.defaultProps = {
+  user: {
+    uid: "test",
+    name: "test-user-name",
+    displayName: "test-display-user-name",
+    email: "sample@sample.com",
+    emailVerified: true,
+    phoneNumber: "00011112222",
+    password: "hash",
+    photoURL: "https://avatars3.githubusercontent.com/u/45778633?s=400&u=d3e6035ae09c6fc39656916113866903c43d73a9&v=4",
+    disabled: false,
+  },
+};
+
+export default Profile;
