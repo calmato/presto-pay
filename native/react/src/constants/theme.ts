441fd333ad5d342fca16301b7b2aea1330f3b24a
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-09-21 16:20:33 +0900

    feat: メインで使用する黒のカラーコードを定義

--- native/react/src/constants/theme.ts
+++ native/react/src/constants/theme.ts
@@ -1,6 +1,7 @@
 export const COLOR = {
   MAIN: "#FFFFFF", // メインカラー (背景等)
   MAIN_LIGHT: "#E5E5E5",
+  MAIN_DARK: "#333333",
   DARK: "#23282F", // メインカラー (ダークモード)
   PRIMARY: "#2BACFC",
   SECONDARY: "#424242",
