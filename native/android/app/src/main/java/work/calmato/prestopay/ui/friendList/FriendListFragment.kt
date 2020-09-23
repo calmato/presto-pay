e568ad0a1bb19157a76966a6cc6403babc3820aa
濵田広大 <hamany0326@gmail.com>
2020-09-23 23:02:33 +0900

    group メンバーの表示

--- native/android/app/src/main/java/work/calmato/prestopay/ui/friendList/FriendListFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/friendList/FriendListFragment.kt
@@ -9,20 +9,16 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_friend_list.*
-import kotlinx.android.synthetic.main.fragment_friend_list.swipeContainer
-import kotlinx.android.synthetic.main.fragment_group_friend.*
 import work.calmato.prestopay.R
 import work.calmato.prestopay.databinding.FragmentFriendListBinding
 import work.calmato.prestopay.network.UserProperty
 import work.calmato.prestopay.network.Users
 import work.calmato.prestopay.util.AdapterCheck
 import work.calmato.prestopay.util.ViewModelFriend
-import work.calmato.prestopay.util.ViewModelGroup
 
 class FriendListFragment : Fragment() {
   private val viewModel  : ViewModelFriend by lazy {
--- native/android/app/src/main/java/work/calmato/prestopay/ui/groupEdit/GroupEditFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/groupEdit/GroupEditFragment.kt
@@ -8,6 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_group_edit.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,27 +21,27 @@ import work.calmato.prestopay.databinding.FragmentGroupEditBinding
 import work.calmato.prestopay.network.Api
 import work.calmato.prestopay.network.GetGroupDetail
 import work.calmato.prestopay.network.GroupPropertyResponse
+import work.calmato.prestopay.network.UserProperty
+import work.calmato.prestopay.util.AdapterFriendPlane
+import work.calmato.prestopay.util.ViewModelFriend
 import work.calmato.prestopay.util.ViewModelGroup
 import kotlin.concurrent.thread
 
 class GroupEditFragment : Fragment() {
-  private var getGroupInfo: GroupPropertyResponse? = null
-  private var groupDetail: GetGroupDetail? = null
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    val binding: FragmentGroupEditBinding =
-      DataBindingUtil.inflate(inflater, R.layout.fragment_group_edit, container, false)
-    getGroupInfo = GroupEditFragmentArgs.fromBundle(requireArguments()).groupEditList
-    return binding.root
+  private val viewModel: ViewModelFriend by lazy {
+    ViewModelProvider(this).get(ViewModelFriend::class.java)
   }
 
+  private var getGroupInfo: GroupPropertyResponse? = null
+  private var groupDetail: GetGroupDetail? = null
+  private var groupMembers: MutableList<UserProperty> = mutableListOf()
   private lateinit var id: String
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  private var recycleAdapter: AdapterFriendPlane? = null
+  private lateinit var friendListArg: List<UserProperty>
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
     thread {
       try {
         Api.retrofitService.getGroupDetail("Bearer $id", getGroupInfo!!.id)
@@ -53,6 +57,10 @@ class GroupEditFragment : Fragment() {
               Log.d(ViewModelGroup.TAG, response.body().toString())
               groupDetail = response.body()
               groupEditName.setText(groupDetail?.name)
+              groupMembers.addAll(groupDetail!!.users)
+              viewModel.friendsList.observe(viewLifecycleOwner, Observer<List<UserProperty>> {
+                recycleAdapter?.friendList = groupMembers
+              })
             }
           })
       } catch (e: Exception) {
@@ -61,7 +69,36 @@ class GroupEditFragment : Fragment() {
     }
     val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
     id = sharedPreferences.getString("token", "")!!
+
   }
+
+
+  private lateinit var clickListener: AdapterFriendPlane.OnClickListener
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val binding: FragmentGroupEditBinding =
+      DataBindingUtil.inflate(inflater, R.layout.fragment_group_edit, container, false)
+    getGroupInfo = GroupEditFragmentArgs.fromBundle(requireArguments()).groupEditList
+
+    clickListener = AdapterFriendPlane.OnClickListener { viewModel.itemIsClicked(it) }
+    recycleAdapter = AdapterFriendPlane(clickListener)
+
+    binding.root.findViewById<RecyclerView>(R.id.friendsRecycleView).apply {
+      layoutManager = LinearLayoutManager(context)
+      adapter = recycleAdapter
+    }
+    return binding.root
+  }
+
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+  }
+
 
   companion object {
     const val TAG = "GroupEditFragment"
