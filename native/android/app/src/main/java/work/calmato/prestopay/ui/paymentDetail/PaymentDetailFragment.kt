b820d74f47bdf93ad55c0d7904c336c648972b8d
Atsuhide <atsuhide.i@gmail.com>
2020-10-03 18:07:18 +0900

    画面遷移後のUIにデータ反映

--- native/android/app/src/main/java/work/calmato/prestopay/ui/paymentDetail/PaymentDetailFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/paymentDetail/PaymentDetailFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_payment_detail.*
 import work.calmato.prestopay.R
 import work.calmato.prestopay.databinding.FragmentPaymentDetailBinding
+import work.calmato.prestopay.network.PaymentPropertyGet
 import work.calmato.prestopay.util.PermissionBase
 
 class PaymentDetailFragment : PermissionBase()  {
+  private lateinit var paymentDetail: PaymentPropertyGet
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -17,6 +21,20 @@ class PaymentDetailFragment : PermissionBase()  {
   ): View? {
     val binding: FragmentPaymentDetailBinding =
       DataBindingUtil.inflate(inflater,R.layout.fragment_payment_detail,container,false)
+    paymentDetail = PaymentDetailFragmentArgs.fromBundle(requireArguments()).paymentPropertyGet
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    date.text = paymentDetail.createdAt.split("T")[0]
+    paymentDetail.imageUrls!![0].let {
+      if(it.isNotEmpty()){
+        Picasso.with(requireContext()).load(it).into(thumbnail)
+      }
+    }
+    expenseName.text = paymentDetail.name
+    amount.text = paymentDetail.total.toString()
+    comment.text = paymentDetail.comment
   }
 }
