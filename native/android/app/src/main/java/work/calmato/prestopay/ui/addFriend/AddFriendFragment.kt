08ff09c2b77c9733c22921f4e14ef4cadacf9f73
Atsuhide <atsuhide.i@gmail.com>
2020-10-10 13:51:07 +0900

    友だち追加での検索バーをSearchViewにし、かつ動的検索にした

--- native/android/app/src/main/java/work/calmato/prestopay/ui/addFriend/AddFriendFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/addFriend/AddFriendFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -17,17 +18,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_add_friend.*
-import kotlinx.android.synthetic.main.fragment_add_friend.nowLoading
 import work.calmato.prestopay.R
 import work.calmato.prestopay.databinding.FragmentAddFriendBinding
 import work.calmato.prestopay.network.UserProperty
 import work.calmato.prestopay.util.*
 
 class AddFriendFragment : Fragment() {
-  private val viewModel : ViewModelFriendGroup by lazy {
+  private val viewModel: ViewModelFriendGroup by lazy {
     ViewModelProvider(this).get(ViewModelFriendGroup::class.java)
   }
-  private var recycleAdapter:AdapterFriendPlane? = null
+  private var recycleAdapter: AdapterFriendPlane? = null
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
@@ -60,35 +60,38 @@ class AddFriendFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    searchFriend.setOnClickListener {
-      val name = userName.text.toString()
-      if (name.isEmpty()){
-        Toast.makeText(requireContext(),resources.getString(R.string.fill_friend_username),Toast.LENGTH_SHORT).show()
-      }else {
-        viewModel.getUserProperties(name, requireActivity())
+    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+      override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
       }
-    }
-    viewModel.usersList.observe(viewLifecycleOwner, Observer {
-        if (it.isEmpty()) {
-          Toast.makeText(requireContext(), resources.getString(R.string.user_not_found), Toast.LENGTH_SHORT).show()
+
+      override fun onQueryTextChange(newText: String?): Boolean {
+        if (newText != null) {
+          if (newText.isNotEmpty()) {
+            viewModel.getUserProperties(newText, requireActivity())
+          }
         }
-    })
-    viewModel.nowLoading.observe(viewLifecycleOwner, Observer {
-      if(it){
-        startHttpConnection(searchFriend,nowLoading,requireContext())
-      }else{
-        finishHttpConnection(searchFriend,nowLoading)
+        return true
       }
     })
-    viewModel.itemClicked.observe(viewLifecycleOwner, Observer {
-      if (null != it) {
+    viewModel.usersList.observe(viewLifecycleOwner, Observer {
+      if (it.isEmpty()) {
+        Toast.makeText(
+          requireContext(),
+          resources.getString(R.string.user_not_found),
+          Toast.LENGTH_SHORT
+        ).show()
+      }
+    })
+    viewModel.itemClicked.observe(viewLifecycleOwner, Observer { userProperty ->
+      if (null != userProperty) {
         val builder: AlertDialog.Builder? = requireActivity().let {
           AlertDialog.Builder(it)
         }
         builder?.setTitle(resources.getString(R.string.add_friend_question))
           ?.setPositiveButton(resources.getString(R.string.add),
             DialogInterface.OnClickListener { _, _ ->
-              viewModel.addFriendApi(it,requireActivity())
+              viewModel.addFriendApi(userProperty, requireActivity())
             })
           ?.setNegativeButton(resources.getString(R.string.cancel), null)
           ?.setView(R.layout.dialog_add_friend)
@@ -96,14 +99,15 @@ class AddFriendFragment : Fragment() {
         dialog?.show()
         val name = dialog?.findViewById<TextView>(R.id.username_dialog)
         val thumbnail = dialog?.findViewById<ImageView>(R.id.thumbnail_dialog)
-        name!!.text = it.name
-        if (it.thumbnailUrl != null && it.thumbnailUrl.isNotEmpty()) {
-          Picasso.with(context).load(it.thumbnailUrl).into(thumbnail)
+        name!!.text = userProperty.name
+        if (userProperty.thumbnailUrl != null && userProperty.thumbnailUrl.isNotEmpty()) {
+          Picasso.with(context).load(userProperty.thumbnailUrl).into(thumbnail)
         }
         viewModel.itemIsClickedCompleted()
       }
     })
   }
+
   companion object {
     internal const val TAG = "AddFriendFragment"
   }
