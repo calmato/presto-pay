23bc52c9b1e246e806a091d661602d8036786771
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-10-11 23:34:06 +0900

    feat: Userエンティティの編集

--- api/calc/internal/domain/user/entity.go
+++ api/calc/internal/domain/user/entity.go
@@ -4,16 +4,17 @@ import "time"
 
 // User - Userエンティティ
 type User struct {
-	ID            string    `json:"id"`                  // Firebase AuthenticationのUID
-	Name          string    `json:"name"`                // 表示名
-	Username      string    `json:"username"`            // ユーザー名 [unique]
-	UsernameLower string    `firestore:"username_lower"` // ユーザー名 (すべて小文字)
-	Email         string    `json:"email"`               // メールアドレス
-	ThumbnailURL  string    `json:"thumbnailUrl"`        // サムネイル
-	GroupIDs      []string  `json:"groupIds"`            // グループID一覧
-	FriendIDs     []string  `firestore:"friend_ids"`     // 友達のユーザーID一覧
-	Password      string    `json:"-"`                   // パスワード
-	InstanceID    string    `firestore:"instance_id"`    // 端末のデバイスID
-	CreatedAt     time.Time `json:"createdAt"`           // 作成日時
-	UpdatedAt     time.Time `json:"updatedAt"`           // 更新日時
+	ID             string    `json:"id"`                    // Firebase AuthenticationのUID
+	Name           string    `json:"name"`                  // 表示名
+	Username       string    `json:"username"`              // ユーザー名 [unique]
+	UsernameLower  string    `firestore:"username_lower"`   // ユーザー名 (すべて小文字)
+	Email          string    `json:"email"`                 // メールアドレス
+	ThumbnailURL   string    `json:"thumbnailUrl"`          // サムネイル
+	GroupIDs       []string  `json:"groupIds"`              // グループID一覧
+	HiddenGroupIds []string  `firestore:"hidden_group_ids"` // 非表示グループID一覧
+	FriendIDs      []string  `firestore:"friend_ids"`       // 友達のユーザーID一覧
+	Password       string    `json:"-"`                     // パスワード
+	InstanceID     string    `firestore:"instance_id"`      // 端末のデバイスID
+	CreatedAt      time.Time `json:"createdAt"`             // 作成日時
+	UpdatedAt      time.Time `json:"updatedAt"`             // 更新日時
 }
