3ee11b3eabe4f5b088f69d7894375dafe624721e
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-10-18 16:52:35 +0900

    feat: OCIのネットワーク部分のTerraform作成

---
+++ infra/terraform/modules/oci/network/network-security-group.tf
@@ -0,0 +1,80 @@
+##################################################
+# ネットワーク・セキュリティ・グループ
+##################################################
+resource "oci_core_network_security_group" "this" {
+  for_each = local.network_security_groups
+
+  display_name   = each.value.name
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.this.id
+
+  freeform_tags  = var.tags
+}
+
+##################################################
+# ネットワーク・セキュリティ・グループ - ルール
+##################################################
+resource "oci_core_network_security_group_security_rule" "this" {
+  for_each = local.network_security_group_rules
+
+  network_security_group_id = each.value.network_security_group_id
+
+  description = each.value.description
+
+  direction = each.value.direction
+  stateless = each.value.stateless
+
+  source_type = each.value.source_type
+  source      = each.value.source
+
+  destination_type = each.value.destination_type
+  destination      = each.value.destination
+
+  protocol = each.value.protocol
+
+  # --- ICMP options ---
+  icmp_options {
+    for_each = each.value.protocol == "1" ? [1] : []
+
+    type = each.value.icmp_type
+    code = each.value.icmp_code
+  }
+
+  # --- UDP options ---
+  dynamic "udp_options" {
+    for_each = each.value.protocol == "17" ? [1] : []
+
+    dynamic "source_port_range" {
+      for_each = each.value.direction == "EGRESS" ? [1] : []
+
+      max = var.value.port_from
+      min = var.value.port_to
+    }
+
+    dynamic "destination_port_range" {
+      for_each = each.value.direction == "INGRESS" ? [1] : []
+
+      max = var.value.port_from
+      min = var.value.port_to
+    }
+  }
+
+  # --- TCP options ---
+  tcp_options {
+    for_each = each.value.protocol == "6" ? [1] : []
+
+    dynamic "source_port_range" {
+      for_each = each.value.direction == "EGRESS" ? [1] : []
+
+      max = var.value.port_from
+      min = var.value.port_to
+    }
+
+    dynamic "destination_port_range" {
+      for_each = each.value.direction == "INGRESS" ? [1] : []
+
+      max = var.value.port_from
+      min = var.value.port_to
+    }
+  }
+}
---
+++ infra/terraform/modules/oci/network/variables.tf
@@ -0,0 +1,82 @@
+##################################################
+# 共通
+##################################################
+variable "compartment_id" {}
+variable "tags" {}
+
+##################################################
+# 仮想クラウド・ネットワーク
+##################################################
+variable "vcn_name" {
+  description = "仮想ネットワーク名"
+  default     = ""
+}
+
+variable "vcn_cidr_block" {
+  description = "CIDRブロック"
+  default     = ""
+}
+
+variable "vcn_dns_label" {
+  description = "DNSラベル (英字と数字のみを含めて、英字で始める必要があります。最大15文字)"
+  default     = ""
+}
+
+##################################################
+# 仮想クラウド・ネットワーク - サブネット
+##################################################
+variable "subnets" {
+  description = "サブネット一覧"
+  type = list(object({
+    name                = string # サブネット名
+    cidr_block          = string # CIDRブロック
+    availability_domain = string # "": リージョナル, "xlwA:AP-TOKYO-AD-1": 可用性ドメイン固有
+    prohibit_public_ip  = bool   # true: プライベートサブネット, false: パブリックサブネット
+  }))
+  default = []
+}
+
+##################################################
+# 仮想クラウド・ネットワーク - インターネット・ゲートウェイ
+##################################################
+variable "igw_name" {
+  description = "インターネットゲートウェイ名"
+  default     = ""
+}
+
+##################################################
+# 仮想クラウド・ネットワーク - ルートテーブル
+##################################################
+variable "public_route_table_name" {
+  description = "ルートテーブル名(パブリックサブネット用)"
+  default     = ""
+}
+
+variable "private_route_table_name" {
+  description = "ルートテーブル名(プライベートサブネット用)"
+  default     = ""
+}
+
+##################################################
+# ネットワーク・セキュリティ・グループ
+##################################################
+# プロトコル番号一覧
+# - ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58")
+# - http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+variable "network_security_groups" {
+  description = "ネットワークセキュリティグループ一覧"
+  type = list(object({
+    name  = string # NSG名
+    rules = list(object({
+      direction   = string # 方向 (INGRESS/EGRESS)
+      stateless   = bool   # ステートレス設定
+      protocol    = string # プロトコル番号 or all
+      source      = string # ソース
+      direstion   = string # 宛先
+      port_from   = number # ポート範囲 (開始位置)
+      port_to     = number # ポート範囲 (終了位置)
+      description = string # NSGルール説明
+    })) # NSGルール一覧
+  }))
+  default = []
+}
