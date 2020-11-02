f221d064e0d58331bce48a1d9912c729002ccdcc
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-11-02 21:09:39 +0900

    style: fmt

--- api/calc/internal/domain/notification.go
+++ api/calc/internal/domain/notification.go
@@ -2,9 +2,9 @@ package domain
 
 // プッシュ通知メッセージ
 const (
-	CreatePaymentNotification = "新しい支払い情報が追加されました."
-	UpdatePaymentNotification = "支払い情報が更新されました."
-	CreateGroupNotification = "新しいグループが作成されました."
-	UpdateGroupNotification = "グループ情報が更新されました."
+	CreatePaymentNotification  = "新しい支払い情報が追加されました."
+	UpdatePaymentNotification  = "支払い情報が更新されました."
+	CreateGroupNotification    = "新しいグループが作成されました."
+	UpdateGroupNotification    = "グループ情報が更新されました."
 	AddUserInGroupNotification = "グループに招待されました."
 )
