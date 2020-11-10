c651b71b35300ce7cfb11d8f1f2876c0221a35d9
濵田広大 <hamany0326@gmail.com>
2020-11-10 11:55:49 +0900

    削除系のUI改善とバグの修正

--- native/android/app/src/main/java/work/calmato/prestopay/ui/groupFriend/GroupFriendFragment.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/ui/groupFriend/GroupFriendFragment.kt
@@ -254,17 +254,16 @@ class GroupFriendFragment : Fragment() {
         )
         val iconMarginVertical =
           (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
-
         deleteIcon.setBounds(
-          itemView.left + iconMarginVertical,
+          itemView.right - iconMarginVertical - deleteIcon.intrinsicWidth,
           itemView.top + iconMarginVertical,
-          itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
+          itemView.right - iconMarginVertical,
           itemView.bottom - iconMarginVertical
         )
         background.setBounds(
-          itemView.left,
+          itemView.right,
           itemView.top,
-          itemView.right + dX.toInt(),
+          itemView.left + dX.toInt(),
           itemView.bottom
         )
         background.draw(c)
@@ -352,15 +351,15 @@ class GroupFriendFragment : Fragment() {
           (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
 
         deleteIcon.setBounds(
-          itemView.left + iconMarginVertical,
+          itemView.right - iconMarginVertical - deleteIcon.intrinsicWidth,
           itemView.top + iconMarginVertical,
-          itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
+          itemView.right - iconMarginVertical,
           itemView.bottom - iconMarginVertical
         )
         background.setBounds(
-          itemView.left,
+          itemView.right,
           itemView.top,
-          itemView.right + dX.toInt(),
+          itemView.left + dX.toInt(),
           itemView.bottom
         )
         background.draw(c)
