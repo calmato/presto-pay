e239af0f923147d06aa32b6beeb9dbee8085fa4b
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-11-15 23:21:59 +0900

    fix: request validation

--- api/calc/internal/application/request/group.go
+++ api/calc/internal/application/request/group.go
@@ -4,22 +4,22 @@ package request
 type CreateGroup struct {
 	Name      string   `json:"name" validate:"required,max=64"`
 	Thumbnail string   `json:"thumbnail"`
-	UserIDs   []string `json:"userIds" validarte:"unique"`
+	UserIDs   []string `json:"userIds" validarte:"max=64,unique"`
 }
 
 // UpdateGroup - グループ編集APIのリクエスト
 type UpdateGroup struct {
 	Name      string   `json:"name" validate:"required,max=64"`
 	Thumbnail string   `json:"thumbnail"`
-	UserIDs   []string `json:"userIds" validarte:"unique"`
+	UserIDs   []string `json:"userIds" validarte:"max=64,unique"`
 }
 
 // AddUsersInGroup - グループへユーザー追加APIのリクエスト
 type AddUsersInGroup struct {
-	UserIDs []string `json:"userIds" validate:"unique"`
+	UserIDs []string `json:"userIds" validate:"max=64,unique"`
 }
 
 // RemoveUsersInGroup - グループのユーザー削除APIのリクエスト
 type RemoveUsersInGroup struct {
-	UserIDs []string `json:"userIds" validate:"unique"`
+	UserIDs []string `json:"userIds" validate:"max=64,unique"`
 }
--- api/calc/internal/application/request/payment.go
+++ api/calc/internal/application/request/payment.go
@@ -5,15 +5,15 @@ import "time"
 // PayerInCreatePayment - 支払い情報作成用 支払い者情報
 type PayerInCreatePayment struct {
 	ID     string  `json:"id" vaidate:"required"`
-	Amount float64 `json:"amount" validate:"gt=-999999,lt=999999"` // TODO: 合計が0になるように
+	Amount float64 `json:"amount" validate:"gt=0,lte=999999"` // TODO: 合計が0になるように
 }
 
 // CreatePayment - 支払い情報作成APIのリクエスト
 type CreatePayment struct {
 	Name           string                  `json:"name" validate:"required,max=64"`
 	Currency       string                  `json:"currency" validate:"required,currency"`
-	PositivePayers []*PayerInCreatePayment `json:"positivePayers" validate:"min=0,max=256"`
-	NegativePayers []*PayerInCreatePayment `json:"negativePayers" validate:"min=0,max=256"`
+	PositivePayers []*PayerInCreatePayment `json:"positivePayers" validate:"min=0,max=64"`
+	NegativePayers []*PayerInCreatePayment `json:"negativePayers" validate:"min=0,max=64"`
 	Tags           []string                `json:"tags" validate:"min=0,max=32,dive,max=32"`
 	Comment        string                  `json:"comment" validate:"max=256"`
 	Images         []string                `json:"images" validate:"min=0,max=32"`
@@ -23,7 +23,7 @@ type CreatePayment struct {
 // PayerInUpdatePayment - 支払い情報編集用 支払い者情報
 type PayerInUpdatePayment struct {
 	ID     string  `json:"id" validate:"required"`
-	Amount float64 `json:"amount" validate:"gt=-999999,lt=999999"` // TODO: 合計が0になるように
+	Amount float64 `json:"amount" validate:"gt=0,lte=999999"` // TODO: 合計が0になるように
 	IsPaid bool    `json:"isPaid" validate:"oneof=true false"`
 }
 
@@ -31,8 +31,8 @@ type PayerInUpdatePayment struct {
 type UpdatePayment struct {
 	Name           string                  `json:"name" validate:"required,max=64"`
 	Currency       string                  `json:"currency" validate:"required,currency"`
-	PositivePayers []*PayerInUpdatePayment `json:"positivePayers" validate:"min=0,max=256"`
-	NegativePayers []*PayerInUpdatePayment `json:"negativePayers" validate:"min=0,max=256"`
+	PositivePayers []*PayerInUpdatePayment `json:"positivePayers" validate:"min=0,max=64"`
+	NegativePayers []*PayerInUpdatePayment `json:"negativePayers" validate:"min=0,max=64"`
 	IsCompleted    bool                    `json:"isCompleted" validate:"oneof=true false"`
 	Tags           []string                `json:"tags" validate:"min=0,max=32,dive,max=32"`
 	Comment        string                  `json:"comment" validate:"max=256"`
