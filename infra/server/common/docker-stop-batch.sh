6828795ba8e13e63357e2f1729b5ef0b55b77664
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-11-21 17:36:43 +0900

    feat: 検証用サーバの設定追加

--- infra/server/common/docker-stop-batch.sh
+++ infra/server/common/docker-stop-batch.sh
@@ -1,2 +1,18 @@
 #!bin/bash
 
+##############################
+# Arguments
+##############################
+container_name="presto_pay_batch"
+
+##############################
+# Main
+##############################
+# 対象のコンテナID一覧を取得
+container_ids=$(docker ps -aq -f "name=$container_name")
+
+# 対象のコンテナの停止
+for i in $container_ids; do
+  docker stop $i
+  echo "stop container: $i"
+done
