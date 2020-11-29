1079bbee2eb900bc20426c802526598a6991c912
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2020-11-29 15:41:05 +0900

    feat: User Serviceに認証なしユーザー作成用のメソッドを追加

--- api/calc/internal/domain/user/service.go
+++ api/calc/internal/domain/user/service.go
@@ -5,5 +5,7 @@ import "context"
 // UserService - UserServiceインターフェース
 type UserService interface {
 	Authentication(ctx context.Context) (*User, error)
+	CreateUnauthorizedUser(ctx context.Context, name string, thumbnail string) (*User, error)
+	UpdateUnauthorizedUser(ctx context.Context, userID string, name string, thumbnail string) (*User, error)
 	ContainsGroupID(ctx context.Context, u *User, groupID string) (bool, error)
 }
