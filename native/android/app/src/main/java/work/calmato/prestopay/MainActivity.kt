6b6ff0005533e041eb0fd18cfbe0eb04b86656c1
Atsuhide <atsuhide.i@gmail.com>
2020-11-29 16:36:49 +0900

    Native adを簡易的に実装

--- native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
@@ -1,8 +1,11 @@
 package work.calmato.prestopay
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
+import com.google.android.gms.ads.formats.NativeAdOptions
+import com.google.android.gms.ads.formats.UnifiedNativeAd
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,38 +16,64 @@ class MainActivity : AppCompatActivity() {
 
     // Initialize Mobile Ads SDK
     MobileAds.initialize(this)
-    val adView = adView
-    val adRequest = AdRequest.Builder().build()
-    adView.loadAd(adRequest)
-    adView.adListener = object : AdListener(){
-      override fun onAdLoaded() {
-        // Code to be executed when an ad finishes loading.
-        adView.visibility = AdView.VISIBLE
-      }
 
-      override fun onAdFailedToLoad(adError : LoadAdError) {
-        // Code to be executed when an ad request fails.
-        adView.visibility = AdView.GONE
-      }
+    // Banner
+//    val adView = adView
+//    val adRequest = AdRequest.Builder().build()
+//    adView.loadAd(adRequest)
+//    adView.adListener = object : AdListener() {
+//      override fun onAdLoaded() {
+//        // Code to be executed when an ad finishes loading.
+//        adView.visibility = AdView.VISIBLE
+//      }
+//
+//      override fun onAdFailedToLoad(adError: LoadAdError) {
+//        // Code to be executed when an ad request fails.
+//        adView.visibility = AdView.GONE
+//      }
+//
+//      override fun onAdOpened() {
+//        // Code to be executed when an ad opens an overlay that
+//        // covers the screen.
+//      }
+//
+//      override fun onAdClicked() {
+//        // Code to be executed when the user clicks on an ad.
+//      }
+//
+//      override fun onAdLeftApplication() {
+//        // Code to be executed when the user has left the app.
+//      }
+//
+//      override fun onAdClosed() {
+//        // Code to be executed when the user is about to return
+//        // to the app after tapping on an ad.
+//      }
+//    }
 
-      override fun onAdOpened() {
-        // Code to be executed when an ad opens an overlay that
-        // covers the screen.
+    // Native ad
+    val adLoader = AdLoader.Builder(this, "ca-app-pub-3940256099942544/6300978111")
+      .forUnifiedNativeAd { ad: UnifiedNativeAd ->
+        // Show the ad.
+        native_ad.setNativeAd(ad)
       }
+      .withAdListener(object : AdListener() {
+        // AdListener callbacks like OnAdFailedToLoad, OnAdOpened, OnAdClicked and
+        // so on, can be overridden here.
+        override fun onAdFailedToLoad(errorCode: Int) {
+          Log.i("MainActivity", "onAdFailedToLoad: $errorCode")
+        }
+      })
+      .withNativeAdOptions(
+        NativeAdOptions.Builder()
+          // Methods in the NativeAdOptions.Builder class can be
+          // used here to specify individual options settings.
+          .build()
+      )
+      .build()
+    adLoader.loadAd(AdRequest.Builder().build())
 
-      override fun onAdClicked() {
-        // Code to be executed when the user clicks on an ad.
-      }
 
-      override fun onAdLeftApplication() {
-        // Code to be executed when the user has left the app.
-      }
-
-      override fun onAdClosed() {
-        // Code to be executed when the user is about to return
-        // to the app after tapping on an ad.
-      }
-    }
   }
 
 }
