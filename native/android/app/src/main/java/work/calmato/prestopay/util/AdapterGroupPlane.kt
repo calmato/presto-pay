cbe15e7bc3434e7fa65a56921053fd56c3822131
Atsuhide <atsuhide.i@gmail.com>
2020-12-01 01:10:03 +0900

    Native ad調整

--- native/android/app/src/main/java/work/calmato/prestopay/util/AdapterGroupPlane.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/util/AdapterGroupPlane.kt
@@ -1,30 +1,37 @@
 package work.calmato.prestopay.util
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.ads.nativetemplates.NativeTemplateStyle
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.formats.NativeAd
 import com.google.android.gms.ads.formats.NativeAdOptions
 import com.google.android.gms.ads.formats.UnifiedNativeAd
+import work.calmato.prestopay.R
 import work.calmato.prestopay.databinding.ListGroupItemPlaneBinding
 import work.calmato.prestopay.network.GroupPropertyResponse
 import work.calmato.prestopay.repository.GroupsRepository
 
-class AdapterGroupPlane(val onClickListener: OnClickListener, val context: Context, val isHidden:Boolean) :
+class AdapterGroupPlane(
+  val onClickListener: OnClickListener,
+  val context: Context,
+  val isHidden: Boolean
+) :
   RecyclerView.Adapter<AdapterGroupPlane.AddGroupViewHolder>() {
   var groupList: List<GroupPropertyResponse> = emptyList()
     set(value) {
       val rawData = value.toMutableList()
-      if(!isHidden){
+      if (!isHidden) {
         val size = value.size
         val divideNum = 7
-        for (i in 1..size/divideNum) {
+        for (i in 1..size / divideNum) {
           rawData.add(
             i * divideNum, GroupPropertyResponse(
               "", "", "", listOf(), "", "",
@@ -33,12 +40,14 @@ class AdapterGroupPlane(val onClickListener: OnClickListener, val context: Conte
             )
           )
         }
-        if(size<divideNum){
-          rawData.add(GroupPropertyResponse(
-            "", "", "", listOf(), "", "",
-            selected = true,
-            isHidden = false
-          ))
+        if (size < divideNum) {
+          rawData.add(
+            GroupPropertyResponse(
+              "", "", "", listOf(), "", "",
+              selected = true,
+              isHidden = false
+            )
+          )
         }
       }
       field = rawData
@@ -53,7 +62,10 @@ class AdapterGroupPlane(val onClickListener: OnClickListener, val context: Conte
       // Native ad
       val adLoader = AdLoader.Builder(context, "ca-app-pub-3940256099942544/2247696110")
         .forUnifiedNativeAd { ad: UnifiedNativeAd ->
+//          val style: NativeTemplateStyle =
+//            NativeTemplateStyle.Builder().withCallToActionTextSize(5f).build()
           // Show the ad.
+//          holder.binding.nativeAd.setStyles(style)
           holder.binding.nativeAd.setNativeAd(ad)
         }
         .withAdListener(object : AdListener() {
