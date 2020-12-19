dba1b6f9b91195d1852b9dd0212227154f9e65ec
Atsuhide <atsuhide.i@gmail.com>
2020-12-19 15:44:15 +0900

    60秒より前に読み込まれた広告は古いので捨てて更新する

--- native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/MainActivity.kt
@@ -1,13 +1,13 @@
 package work.calmato.prestopay
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.formats.NativeAdOptions
 import com.google.android.gms.ads.formats.UnifiedNativeAd
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,16 +18,24 @@ class MainActivity : AppCompatActivity() {
     // Initialize Mobile Ads SDK
     MobileAds.initialize(this)
     refreshAd()
+    refreshAdFlag.value = false
+    nativeAdCountDownTimer()
   }
   companion object {
     var nativeAd : UnifiedNativeAd? = null
+    var refreshAdFlag : MutableLiveData<Boolean> = MutableLiveData(true)
   }
 
   fun refreshAd() {
+    Log.i("MainActivityRefresh", "refreshAd: ")
+    nativeAd?.destroy()
     // Native ad
     val adLoader = AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
       .forUnifiedNativeAd { ad: UnifiedNativeAd ->
         nativeAd = ad
+        // Timer
+        refreshAdFlag.value = false
+        nativeAdCountDownTimer()
       }
       .withAdListener(object : AdListener() {
         // AdListener callbacks like OnAdFailedToLoad, OnAdOpened, OnAdClicked and
@@ -44,5 +52,15 @@ class MainActivity : AppCompatActivity() {
       )
       .build()
     adLoader.loadAd(AdRequest.Builder().build())
+  }
+
+  fun nativeAdCountDownTimer(){
+    object : CountDownTimer(61000, 1000) {
+      override fun onTick(millisUntilFinished: Long) {
+      }
+      override fun onFinish() {
+        refreshAdFlag.value = true
+      }
+    }.start()
   }
 }
