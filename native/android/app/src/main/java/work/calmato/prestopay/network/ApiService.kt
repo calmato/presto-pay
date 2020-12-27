561fc52523140108f400bdf9a44a685e1df6baab
Atsuhide <atsuhide.i@gmail.com>
2020-12-27 21:50:29 +0900

    fix: Altered keys
    Signed-off-by: Atsuhide <atsuhide.i@gmail.com>

--- native/android/app/src/main/java/work/calmato/prestopay/network/ApiService.kt
+++ native/android/app/src/main/java/work/calmato/prestopay/network/ApiService.kt
@@ -10,13 +10,13 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
+import work.calmato.prestopay.BuildConfig
 import java.security.cert.X509Certificate
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 
-private const val BASE_URL_DEV = "https://api.presto-pay-dev.calmato.work/v1/"
-private const val BASE_URL_STG = "https://api.presto-pay-stg.calmato.work/v1/"
+private const val BASE_URL = BuildConfig.BASE_URL
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
  * full Kotlin compatibility.
@@ -60,7 +60,7 @@ fun getUnsafeOkHttpClient(): OkHttpClient {
 private val retrofit = Retrofit.Builder()
   .addConverterFactory(MoshiConverterFactory.create(moshi))
   .addCallAdapterFactory(CoroutineCallAdapterFactory())
-  .baseUrl(BASE_URL_STG)
+  .baseUrl(BASE_URL)
   .client(getUnsafeOkHttpClient())
   .build()
 
